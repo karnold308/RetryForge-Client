6ed194a66e4ce831cb16f8ee20e85a96d8da3d9f
karnold308 <karnold308@gmail.com>
2026-05-13 15:11:12 -0400

    debugging google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,8 @@ import { initGA } from './utils/analytics';
 if (process.env.REACT_APP_ENV === 'production') {
   // Initialize Google Analytics
   initGA();
+} else {
+  console.log(process.env.REACT_APP_ENV)
 }
 
 createRoot(document.getElementById('root')!).render(

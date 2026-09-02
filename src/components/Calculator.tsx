7271035238b83119aa7f6bcd1b0a5ffc926ad7b7
karnold308 <karnold308@gmail.com>
2026-09-02 12:20:57 -0400

    fixng bug with calculator.

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -23,7 +23,7 @@ export default function Calculator() {
         const feeRate = 0.05;
         const failRevenue = Number(inputMMRValue) * failureRate;
         const weRecov = failRevenue * recoveryRate;
-        const ourFee = 49 + weRecov * feeRate;
+        const ourFee = (49 * 12) + weRecov * feeRate;
         const totalGain = weRecov - ourFee;
 
         setCalcResult({
@@ -141,7 +141,7 @@ export default function Calculator() {
                                         +${null != calcResult ? formatCompactNumber(calcResult.recoverable) : 0}/yr
                                     </strong>
                                     <p className="mt-2.5 text-sm text-gray-500">
-                                        ≈ +${null != calcResult ? formatCurrency(calcResult.netGain) : 0}/mo net after fees
+                                        ≈ +${null != calcResult ? formatCurrency(calcResult.netGain) : 0}/yr net after fees
                                     </p>
                                 </div>
                             </div>
